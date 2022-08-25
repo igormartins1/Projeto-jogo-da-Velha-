e380d66b07a55be4d406a58dae81178d8bea42c8
IgorMartins01 <igor.almeida@rbmweb.com.br>
2022-08-25 13:53:29 -0300

    Montando a Logica

--- js/scripts.js
+++ js/scripts.js
@@ -76,14 +76,50 @@ function checkEl(player1,player2){
 
 function checkinCondicional(){
     let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
-    let b1=document.getElementById("block-1")
+    let b2=document.getElementById("block-2")
+    let b3=document.getElementById("block-3")
+    let b4=document.getElementById("block-4")
+    let b5=document.getElementById("block-5")
+    let b6=document.getElementById("block-6")
+    let b7=document.getElementById("block-7")
+    let b8=document.getElementById("block-8")
+    let b9=document.getElementById("block-9")
+
+    if(b1.childNodes.length>0 && b2.childNodes.length>0 && b3.childNodes.length>0){
+        let b1child=b1.childNodes[0].className;
+        let b2child=b2.childNodes[0].className;
+        let b3child=b3.childNodes[0].className;
+
+        if(b1child=='x' && b2child=='x' && b3child=='x'){
+            //Quando o x venceu 
+
+        } else if(b1child=='o' && b2child=='o' && b3child=='o'){
+            // Quando o o venceu
+        }
+    }
+
+    
+    if(b4.childNodes.length>0 && b5.childNodes.length>0 && b6.childNodes.length>0){
+        let b4child=b4.childNodes[0].className;
+        let b5child=b5.childNodes[0].className;
+        let b6child=b6.childNodes[0].className;
+
+        if(b4child=='x' && b5child=='x' && b6child=='x'){
+            //Quando o x venceu 
+            console.log('x venceu');
+
+        } else if(b4child=='o' && b5child=='o' && b6child=='o'){
+            // Quando o o venceu
+            console.log('o venceu');
+        }
+    }
+
+
+
+
+
+
+
 }
 
 
